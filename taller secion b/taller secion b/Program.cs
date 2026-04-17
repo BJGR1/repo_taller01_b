05b11b744834fb55da51f4f32d620072af6f2505
BJGR1 <lucibray2717@gmail.com>
2026-04-17 14:54:15 -0400

    hicimos repositorio

--- taller secion b/taller secion b/Program.cs
+++ taller secion b/taller secion b/Program.cs
@@ -1,12 +1,13 @@
 ﻿/*
  * Creado por SharpDevelop.
- * Usuario: Usuario
+ * Usuario: brayhan garcia
  * Fecha: 17/4/2026
  * Hora: 2:26 p. m.
  * 
  * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
  */
 using System;
+using System.IO;
 
 namespace taller_secion_b
 {
@@ -16,8 +17,21 @@ namespace taller_secion_b
 		{
 			Console.WriteLine("ya lo hice");
 			
-			// TODO: Implement Functionality Here
+			//  directorio
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"DATOSIUJO");
 			
+			string rutaReportes =Path.Combine(rutaRaiz, "Reportes");
+			
+			Console.WriteLine(rutaRaiz);
+			Console.WriteLine(rutaReportes);
+			if (!Directory.Exists(rutaReportes)){
+				//crear el directorio reportes
+				Directory.CreateDirectory(rutaReportes);
+				Console.WriteLine("directorio creado correctamente");
+			    
+			}
+
+
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
